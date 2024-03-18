7c8de32f6e35406ca97462995d8064039a116b1c
TudorCosmin <cosminoanea01@gmail.com>
2024-03-18 18:59:24 +0200

    all zones implemented

--- agent.py
+++ agent.py
@@ -1,13 +1,15 @@
 from uagents import Agent, Context, Bureau
 import zone_upleft
 import zone_upright
+import zone_bottomleft
+import zone_bottomright
 import police
 import environment
 import random
 
 bad_guy = Agent(name="bad", seed="baaaad")
 
-@bad_guy.on_interval(period=5.0)
+@bad_guy.on_interval(period=0.5)
 async def spawn_random(ctx: Context):
     rows = len(environment.matrix)
     cols = len(environment.matrix[0])
@@ -19,17 +21,29 @@ async def spawn_random(ctx: Context):
 def init_all():
     zone_upleft.radar_upleft._storage.set("i", 0)
     zone_upleft.radar_upleft._storage.set("j", 0)
-    zone_upleft.radar_upleft._storage.set("i_limit", 10)
+    zone_upleft.radar_upleft._storage.set("i_limit", 5)
     zone_upleft.radar_upleft._storage.set("j_limit", 5)
 
-    zone_upright.radar_upright._storage.set("i", 0)
+    zone_upright.radar_upright._storage.set("i", 5)
     zone_upright.radar_upright._storage.set("j", 5)
     zone_upright.radar_upright._storage.set("i_limit", 10)
     zone_upright.radar_upright._storage.set("j_limit", 10)
 
+    zone_bottomleft.radar_bottomleft._storage.set("i", 5)
+    zone_bottomleft.radar_bottomleft._storage.set("j", 0)
+    zone_bottomleft.radar_bottomleft._storage.set("i_limit", 10)
+    zone_bottomleft.radar_bottomleft._storage.set("j_limit", 5)
+
+    zone_bottomright.radar_bottomright._storage.set("i", 5)
+    zone_bottomright.radar_bottomright._storage.set("j", 5)
+    zone_bottomright.radar_bottomright._storage.set("i_limit", 10)
+    zone_bottomright.radar_bottomright._storage.set("j_limit", 10)
+
 bureau = Bureau()
 bureau.add(zone_upleft.radar_upleft)
 bureau.add(zone_upright.radar_upright)
+bureau.add(zone_bottomleft.radar_bottomleft)
+bureau.add(zone_bottomright.radar_bottomright)
 bureau.add(police.police_agent)
 bureau.add(bad_guy)
 
---
+++ zone_bottomright.py
@@ -0,0 +1,30 @@
+from uagents import Agent, Context, Bureau, Model
+from enum import Enum
+import random
+import time
+import environment
+import police
+
+radar_bottomright = Agent(name="radar_bottomright", seed="radar_bottomright recovery phrase")
+
+@radar_bottomright.on_interval(period=0.1)
+async def scan_area(ctx: Context):
+    i = ctx.storage.get("i")
+    j = ctx.storage.get("j")
+    if environment.matrix[i][j] == environment.Guys.BAD_GUY:
+        ctx.logger.info("bad guy found! calling for HELP: ({}, {})".format(i, j))
+        await ctx.send(police.police_agent.address, environment.NeedHelp(coordinates=(i, j)))
+    else:
+        ctx.logger.info("not bad guy at: ({}, {})".format(i, j))
+    
+    # go to next area
+    if j + 1 < ctx.storage.get("j_limit"):
+        ctx.storage.set("j", j + 1)
+        # print(j)
+    else:
+        if i + 1 < ctx.storage.get("i_limit"):
+            ctx.storage.set("i", i + 1)
+            ctx.storage.set("j", 5)
+        else:
+            ctx.storage.set("i", 5)
+            ctx.storage.set("j", 5)
